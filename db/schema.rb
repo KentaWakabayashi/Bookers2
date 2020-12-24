eabfc4c532a00bc0fa2719bb05f25f759a367bbb
KentaWakabayashi <kenken050628@gmail.com>
2020-12-24 07:52:31 +0000

    再提出

--- db/schema.rb
+++ db/schema.rb
@@ -15,9 +15,9 @@ ActiveRecord::Schema.define(version: 2020_12_20_070113) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "body"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
   end
 
   create_table "post_images", force: :cascade do |t|
